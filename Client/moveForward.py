3baea705852ec6159998c1c6312129d30710d6b2
Pyrolin <113906792+Pyrolin@users.noreply.github.com>
2024-06-06 11:57:54 +0200

    Created moveForward

---
+++ Client/moveForward.py
@@ -0,0 +1,18 @@
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port
+
+speed = 500
+
+# Initialize brick
+ev3 = EV3Brick()
+
+# Initialize motors
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.B)
+
+def move_forward():
+    left_motor.run(speed)
+    right_motor.run(speed)
+
+move_forward()
