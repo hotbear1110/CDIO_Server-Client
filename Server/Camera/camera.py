55a566cbe4a5533dbf04d27840708c3d5e70e42d
Mads Hauberg <s224307@dtu.dk>
2024-06-21 12:08:34 +0200

    Fixed object coordinates and added logic to obstacle
    getObstacle now tries to figure out the orientation of the cross

--- Server/Camera/camera.py
+++ Server/Camera/camera.py
@@ -55,15 +55,15 @@ class Grid:
 
         self.boxes = [[Box() for j in range(self.rows)] for i in range(self.cols)]
 
-        self.robot = (0, 0)
-        self.robotFront = (0, 0)
-        self.robotBack = (0, 0)
-        self.egg = (0, 0)
-        self.obstacle = (0, 0)
-        self.oBall = (0, 0)
+        self.robot = (0, 0, 0, 0)
+        self.robotFront = (0, 0, 0, 0)
+        self.robotBack = (0, 0, 0, 0)
+        self.egg = (0, 0, 0, 0)
+        self.obstacle = (0, 0, 0, 0)
+        self.oBall = (0, 0, 0, 0)
         self.wBalls = []
-        self.goalSmall = (0, 0)
-        self.goalLarge = (0, 0)
+        self.goalSmall = (0, 0, 0, 0)
+        self.goalLarge = (0, 0, 0, 0)
 
     def getRobot(self):
         return self.robot
@@ -78,7 +78,37 @@ class Grid:
         return self.egg
     
     def getObstacle(self):
-        return self.obstacle
+        egg_x1, egg_y1, egg_x2, egg_y2 = self.egg
+        obstacle_x1, obstacle_y1, obstacle_x2, obstacle_y2 = self.obstacle
+
+        cross1 = (0, 0, 0, 0)
+        cross2 = (0, 0, 0, 0)
+
+        
+        obstacle_width = obstacle_x2 - obstacle_x1
+        obstacle_height = obstacle_y2 - obstacle_y1
+
+        #if egg is inside the obstacle
+        if ((egg_x2 > obstacle_x1) and (egg_x1 < obstacle_x2)) and ((egg_y2 > obstacle_y1) and (egg_y1 < obstacle_y2)):
+            #if egg in the x middel
+            if (egg_x1 < (obstacle_x2 - obstacle_width/2)) and (egg_x2 > (obstacle_x2 - obstacle_width/2)):
+                cross1 = (obstacle_x1 + obstacle_width/2, obstacle_y1, obstacle_x2 - obstacle_width/2, obstacle_y2)
+                
+                cross2 = (obstacle_x1, obstacle_y1 + obstacle_height/2, obstacle_x2, obstacle_y2 - obstacle_height/2)
+            else:
+                #if egg is in the y middle
+                if (egg_y1 < (obstacle_y2 - obstacle_height/2)) and (egg_y2 > (obstacle_y2 - obstacle_height/2)):
+                    cross1 = self.obstacle
+
+                    cross2 = (obstacle_x2, obstacle_y1, obstacle_x1, obstacle_y2)
+                else:
+                    cross1 = (obstacle_x1 + obstacle_width/2, obstacle_y1, obstacle_x2 - obstacle_width/2, obstacle_y2)
+                    
+                    cross2 = (obstacle_x1, obstacle_y1 + obstacle_height/2, obstacle_x2, obstacle_y2 - obstacle_height/2)
+
+
+        return [cross1, cross2]
+    
     def getMidpoint(self, obstacle):
         midpoint_x = (obstacle[0][0] + obstacle[1][0]) / 2
         midpoint_y = (obstacle[0][1] + obstacle[1][1]) / 2
@@ -98,6 +128,26 @@ class Grid:
         
 
     def addBox(self, pos_x1, pos_y1, pos_x2, pos_y2, name):
+
+        if name == "WBall":
+            self.wBalls.append((pos_x1, pos_y1, pos_x2, pos_y2))
+        elif name == "OBall":
+            self.oBall = ((pos_x1, pos_y1, pos_x2, pos_y2))
+        elif name == "Egg":
+            self.egg = ((pos_x1, pos_y1, pos_x2, pos_y2))
+        elif name == "robot":
+            self.robot = ((pos_x1, pos_y1, pos_x2, pos_y2))
+        elif name == "robotFront":
+            self.robotFront = ((pos_x1, pos_y1, pos_x2, pos_y2))
+        elif name == "robotBack":
+            self.robotBack = ((pos_x1, pos_y1, pos_x2, pos_y2))
+        elif name == "Goal-Small-":
+            self.goalSmall = ((pos_x1, pos_y1, pos_x2, pos_y2))
+        elif name == "Goal-Large-":
+            self.goalLarge = ((pos_x1, pos_y1, pos_x2, pos_y2))
+        elif name == "Obstacle":
+            self.obstacle = ((pos_x1, pos_y1, pos_x2, pos_y2))
+
         x_boxes = math.ceil((self.res_x-pos_x1)/self.precision)-(math.floor((self.res_x-pos_x2)/self.precision))
         y_boxes = math.ceil((self.res_y-pos_y1)/self.precision)-(math.floor((self.res_y-pos_y2)/self.precision))
         positions = []
@@ -106,29 +156,7 @@ class Grid:
             for y in range(y_boxes):
                 position = [min(math.ceil(max(pos_x1, 1)/self.precision)+x, self.cols - 1), min(math.ceil(max(pos_y1, 1)/self.precision)+y, self.rows - 1)]
 
-                if name == "WBall":
-                   self.wBalls.append((position[0], position[1]))
-                elif name == "OBall":
-                    self.oBall = ((position[0], position[1]))
-                elif name == "Egg":
-                    self.egg = ((position[0], position[1]))
-                elif name == "robot":
-                    self.robot = ((position[0], position[1]))
-                elif name == "robotFront":
-                    self.robotFront = ((position[0], position[1]))
-                elif name == "robotBack":
-                    self.robotBack = ((position[0], position[1]))
-                elif name == "Goal-Small-":
-                    self.goalSmall = ((position[0], position[1]))
-                elif name == "Goal-Large-":
-                    self.goalLarge = ((position[0], position[1]))
-                elif name == "Obstacle":
-                    self.obstacle = ((position[0], position[1]))
-
                 if (name != "Wall" or self.boxes[position[0]][position[1]].getName() == "") and (self.boxes[position[0]][position[1]].getName() != "Egg"):
-                    if(name)=="Obstacle":
-                        obstacle=[]
-
 
                     self.boxes[position[0]][position[1]].updateName(name)
 
